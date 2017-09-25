4e56f69ddc751c29aa09a6f147fc15e9e13e93b2
nipasarker01 <nipasarker.hd1@gmail.com>
2017-09-25 19:45:42 +0530

    Add files via upload

--- App/AppContainer.js
+++ App/AppContainer.js
@@ -1,76 +1,31 @@
-import React, { Component } from 'react'
-import { Drawer, View } from 'native-base'
-import {DrawerNavigator} from 'react-navigation'
-
-import SplashScene from './scenes/splashScene'
-import OnboardingScene from './scenes/onboardingScene'
-import LoginScene from './scenes/loginScene'
-import SignupScene from './scenes/signupScene'
-
-const settings = new SettingsStore()
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from './scenes/splashScene';
+import AuthContainer from './components/AuthContainer/AuthContainer';
+import LoginContainer from './components/AuthContainer/LoginContainer/loginContainer';
 
 
-export default class AppContainer extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      toggled: false,
-      store: {
-        settings: settings,
-      },
-
-    }
-  }
-  toggleDrawer() {
-    this.state.toggled ? this._drawer.close() : this._drawer.open()
-  }
-  openDrawer() {
-    this.setState({toggled: true})
-  }
-  closeDrawer() {
-    this.setState({toggled: false})
-  }
-  renderScene(route, navigator) {
-    switch(route.title) {
-      case 'Splash': {
-        return <SplashScene {...route.passProps} navigator={navigator}/>
-      }
-      case 'Login': {
-        return <LoginScene {...route.passProps} navigator={navigator} />
-      }
-
-      default: {
-        return null
-      }
-    }
-  }
-  configureScene(route, routeStack) {
-    return Navigator.SceneConfigs.PushFromRight
-  }
+export default class App extends React.Component {
   render() {
     return (
-      <Drawer
-        ref={(ref) => this._drawer = ref}
-        type="displace"
-        content={<SideMenu toggleDrawer={this.toggleDrawer.bind(this)} stores={this.state.store} navigator={this._navigator} theme={this.state.theme}/>}
-        onClose={this.closeDrawer.bind(this)}
-        onOpen={this.openDrawer.bind(this)}
-        openDrawerOffset={0.2}
-        >
-          <Navigator
-            ref={(ref) => this._navigator = ref}
-            configureScene={this.configureScene.bind(this)}
-            renderScene={this.renderScene.bind(this)}
-            initialRoute={{
-              title: "Splash",
-              passProps: {
-                stores: this.state.store,
-                toggleDrawer: this.toggleDrawer.bind(this),
-                theme: this.state.theme
-              }
-            }}
-            />
-        </Drawer>
-    )
+      <SplashScreen>
+
+    <AuthContainer>
+
+  
+
+    </AuthContainer>
+
+     </SplashScreen>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+}
+});
